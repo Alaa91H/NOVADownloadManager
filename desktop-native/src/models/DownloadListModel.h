2206e8727500e96b75abdc113cedc40a4b4e9685
Alaa <alahus2591@gmail.com>
2026-09-24 03:06:47 +0200

    native-ui: add native downloads list model

---
+++ desktop-native/src/models/DownloadListModel.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <QAbstractListModel>
+#include <QJsonArray>
+#include <QString>
+#include <QVector>
+
+class DownloadListModel final : public QAbstractListModel {
+    Q_OBJECT
+    Q_PROPERTY(int activeCount READ activeCount NOTIFY summaryChanged)
+    Q_PROPERTY(qint64 totalSpeed READ totalSpeed NOTIFY summaryChanged)
+
+public:
+    enum Role {
+        IdRole = Qt::UserRole + 1,
+        NameRole,
+        StatusRole,
+        SizeBytesRole,
+        DownloadedBytesRole,
+        ProgressRole,
+        SpeedRole,
+        EtaRole,
+        SavePathRole,
+        EngineRole
+    };
+    Q_ENUM(Role)
+
+    explicit DownloadListModel(QObject *parent = nullptr);
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QHash<int, QByteArray> roleNames() const override;
+
+    int activeCount() const noexcept;
+    qint64 totalSpeed() const noexcept;
+
+public slots:
+    void replaceFromJson(const QJsonArray &downloads);
+
+signals:
+    void summaryChanged();
+
+private:
+    struct Item {
+        QString id;
+        QString name;
+        QString status;
+        qint64 sizeBytes{0};
+        qint64 downloadedBytes{0};
+        qreal progress{0.0};
+        qint64 speedBytesPerSec{0};
+        int etaSeconds{0};
+        QString savePath;
+        QString engine;
+    };
+
+    QVector<Item> m_items;
+};
