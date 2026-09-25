0a36ae18ca9121ebfd6345037259eda8a96bb58d
Alaa <alahus2591@gmail.com>
2026-09-25 08:19:36 +0200

    feat(native-ui): expose live display metrics

--- desktop-native/src/platform/DesktopIntegration.h
+++ desktop-native/src/platform/DesktopIntegration.h
@@ -2,13 +2,16 @@
 
 #include <QJsonArray>
 #include <QObject>
+#include <QMetaObject>
 #include <QString>
 #include <QVariantMap>
 
 class QWindow;
+class QScreen;
 
 class DesktopIntegration final : public QObject {
     Q_OBJECT
+    Q_PROPERTY(QVariantMap displayMetrics READ displayMetrics NOTIFY displayMetricsChanged)
 
 public:
     explicit DesktopIntegration(QObject *parent = nullptr);
@@ -19,6 +22,7 @@ public:
     Q_INVOKABLE bool revealInFolder(const QString &path);
     Q_INVOKABLE bool openExternalUrl(const QString &url);
     Q_INVOKABLE QVariantMap browserNativeHostStatus() const;
+    Q_INVOKABLE QVariantMap displayMetrics() const;
     Q_INVOKABLE bool repairBrowserNativeHost();
     Q_INVOKABLE QString readClipboardText() const;
     Q_INVOKABLE QString chooseDirectory(const QString &initialDirectory = QString());
@@ -35,15 +39,18 @@ public:
     void handleDownloads(const QJsonArray &downloads);
 
 signals:
+    void displayMetricsChanged();
     void operationSucceeded(const QString &action);
     void operationFailed(const QString &action, const QString &message);
 
 private:
+    void watchScreen(QScreen *screen);
     bool openLocalUrl(const QString &action, const QString &path);
     void fail(const QString &action, const QString &message);
     void setDesktopProgress(bool visible, bool indeterminate, qreal progress, int activeCount);
 
     QWindow *m_window{nullptr};
+    QMetaObject::Connection m_windowScreenConnection;
 #if defined(Q_OS_WIN)
     void *m_taskbar{nullptr};
     bool m_comInitialized{false};
